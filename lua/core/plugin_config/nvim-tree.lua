5274fa491717d0bbf605bafc5367177ef2274af6
arashjp8 <arashjafarpour723@gmail.com>
2024-05-31 14:53:51 +0330

    [checkpoint] - file tree toggle keymap added

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -4,4 +4,6 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup({
 })
 
-vim.keymap.set('n', '<c-n>', ':NvimTreeFindFile<CR>')
+vim.keymap.set('n', '<c-t>', ':NvimTreeFindFile<CR>')
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+
