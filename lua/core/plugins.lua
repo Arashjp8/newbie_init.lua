b3e78d24071f2506af0dd978088550e9a8496f63
arashjp8 <arashjafarpour723@gmail.com>
2024-05-31 12:41:27 +0330

    [checkpoint] - dark++ vscode theme added

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,7 +13,8 @@ local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use ({ "catppuccin/nvim", as = "catppuccin", config = function() vim.cmd('colorscheme catppuccin') end })
+  use ({ "rockyzhang24/arctic.nvim",
+  requires = { "rktjmp/lush.nvim" }, config = function() vim.cmd('colorscheme arctic') end })
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
